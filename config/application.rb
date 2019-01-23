0983bb39350d82ba6935cba8803c993f1f80988d
spensation <spensation@gmail.com>
2019-01-23 14:50:16 -0800

    Configure and create helpers - Change include_all_helpers config to false - Split the _collapsible_elements.html.erb file's content into partials and extract logic from the file into partials

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Books
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    # Overrides the default setting that loads all helpers to all views
+    config.action_controller.include_all_helpers = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
